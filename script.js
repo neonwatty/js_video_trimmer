52bde5438fcb1eb4dfe4c78187d7d05a47f95f6e
Jeremy Watt <jermwatt@gmail.com>
2024-10-02 10:22:24 -0700

    slider bounded

--- script.js
+++ script.js
@@ -1,45 +1,66 @@
-// Make the DIV element draggable:
-dragElement(document.getElementById("trim-start-drag"));
+window.addEventListener(
+  "load",
+  () => {
+    // define elements
+    const trimStartDrag = document.getElementById("trim-start-drag");
+    const videoScanner = document.getElementById("video-scanner");
 
-function dragElement(elmnt) {
-  let pos1 = 0,
-    pos2 = 0,
-    pos3 = 0,
-    pos4 = 0;
+    // get bounding x values from scanner box
+    const widthEndOffset = 12;
+    const scannerWidth = videoScanner.offsetWidth - widthEndOffset;
 
-  elmnt.onmousedown = dragMouseDown;
+    // Make the DIV element draggable:
+    dragElement(trimStartDrag);
 
-  function dragMouseDown(e) {
-    e = e || window.e;
-    e.preventDefault();
+    function dragElement(elmnt) {
+      let pos1 = 0,
+        pos2 = 0,
+        pos3 = 0,
+        pos4 = 0;
 
-    // Get the mouse cursor position at startup:
-    pos3 = e.clientX;
-    pos4 = e.clientY;
-    document.onmouseup = closeDragElement;
+      elmnt.onmousedown = dragMouseDown;
 
-    // Call a function whenever the cursor moves:
-    document.onmousemove = elementDrag;
-  }
+      function dragMouseDown(e) {
+        e = e || window.e;
+        e.preventDefault();
 
-  function elementDrag(e) {
-    e = e || window.e;
-    e.preventDefault();
+        // Get the mouse cursor position at startup:
+        pos3 = e.clientX;
+        pos4 = e.clientY;
+        document.onmouseup = closeDragElement;
 
-    // Calculate the new cursor position:
-    pos1 = pos3 - e.clientX;
-    pos2 = pos4 - e.clientY;
-    pos3 = e.clientX;
-    pos4 = e.clientY;
+        // Call a function whenever the cursor moves:
+        document.onmousemove = elementDrag;
+      }
 
-    // Set the element's new position:
-    elmnt.style.left = elmnt.offsetLeft - pos1 + "px";
-    // elmnt.style.top = elmnt.offsetTop - pos2 + "px"; // Update vertical position
-  }
+      function elementDrag(e) {
+        e = e || window.e;
+        e.preventDefault();
 
-  function closeDragElement() {
-    // Stop moving when mouse button is released:
-    document.onmouseup = null;
-    document.onmousemove = null;
-  }
-}
+        // Calculate the new cursor position:
+        pos1 = pos3 - e.clientX;
+        pos2 = pos4 - e.clientY;
+        pos3 = e.clientX;
+        pos4 = e.clientY;
+
+        // Set the element's new position:
+        let newX = elmnt.offsetLeft - pos1;
+        if (newX / scannerWidth > 1) {
+          newX = scannerWidth;
+        } else if (newX < 0) {
+          newX = 0;
+        }
+
+        elmnt.style.left = newX + "px";
+        // elmnt.style.top = elmnt.offsetTop - pos2 + "px"; // Update vertical position
+      }
+
+      function closeDragElement() {
+        // Stop moving when mouse button is released:
+        document.onmouseup = null;
+        document.onmousemove = null;
+      }
+    }
+  },
+  500
+);
