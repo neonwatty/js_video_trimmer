78e68b2b1ccf78b4cd0f2efdace37a0e59067444
Jeremy Watt <jermwatt@gmail.com>
2024-10-06 06:04:02 -0700

    play button works

--- controls.js
+++ controls.js
@@ -5,24 +5,6 @@ import { inputVideo, onDurationReady } from "./video.js";
 let volumeControl = document.getElementById("volume-control");
 let volumeButton = document.getElementById("volume-button");
 
-// show/hide volume controls
-volumeButton.addEventListener("click", () => {
-  volumeControl.classList.toggle("collapse");
-
-  if (!volumeControl.classList.contains("collapse")) {
-    volumeControl.addEventListener("change", setVolume);
-    volumeControl.addEventListener("input", setVolume);
-  } else {
-    volumeControl.removeEventListener("change", setVolume);
-    volumeControl.removeEventListener("input", setVolume);
-  }
-});
-
-// volume setter
-function setVolume() {
-  inputVideo.volume = this.value / 100;
-}
-
 // scanner elements
 const trimStartDrag = document.getElementById("trim-start-drag");
 const videoScanner = document.getElementById("video-scanner");
@@ -32,21 +14,6 @@ const playPauseButton = document.getElementById("play-pause-button");
 const playFilledButton = document.getElementById("play-button-filled");
 const playEmptyButton = document.getElementById("play-button-empty");
 const pauseEmptyButton = document.getElementById("pause-button-empty");
-
-let videoPlayIndicator = true;
-
-playPauseButton.addEventListener("click", () => {
-  playFilledButton.classList.toggle("hidden");
-  playEmptyButton.classList.toggle("hidden");
-  pauseEmptyButton.classList.toggle("hidden");
-  if (videoPlayIndicator) {
-    inputVideo.play();
-    videoPlayIndicator = false;
-  } else {
-    inputVideo.pause();
-    videoPlayIndicator = true;
-  }
-});
 
 function waitForVideoDuration() {
   return new Promise((resolve) => {
@@ -63,20 +30,73 @@ window.addEventListener(
     let videoDuration;
     try {
       videoDuration = await waitForVideoDuration();
-      console.log("Video duration is ready:", videoDuration);
     } catch (error) {
       console.error("Error waiting for video duration:", error);
     }
-
-    // get bounding x values from scanner box
-    const widthEndOffset = 12;
-    const scannerWidth = videoScanner.offsetWidth - widthEndOffset;
 
     // map scanner box width to videoDuration
     function scannerWidthDurationMapper(t) {
       const normalizedVal = (t / scannerWidth) * videoDuration;
       return normalizedVal;
     }
+
+    // map videuDuration to scanner box width
+    function inverseScannerWidthDurationMapper(t) {
+      const normalizedVal = (t / videoDuration) * scannerWidth;
+      return normalizedVal;
+    }
+
+    // show/hide volume controls
+    volumeButton.addEventListener("click", () => {
+      volumeControl.classList.toggle("collapse");
+
+      if (!volumeControl.classList.contains("collapse")) {
+        volumeControl.addEventListener("change", setVolume);
+        volumeControl.addEventListener("input", setVolume);
+      } else {
+        volumeControl.removeEventListener("change", setVolume);
+        volumeControl.removeEventListener("input", setVolume);
+      }
+    });
+
+    // volume setter
+    function setVolume() {
+      inputVideo.volume = this.value / 100;
+    }
+
+    // visual scanner update
+    function setScanner() {
+      // update actual time of scanner
+      const newX = inputVideo.currentTime;
+      saveSlider("start_slider_video", newX);
+
+      // update visual scanner
+      const scannerNormalizedNewX = inverseScannerWidthDurationMapper(newX);
+      saveSlider("start_slider_actual", scannerNormalizedNewX);
+      trimStartDrag.style.left = Math.ceil(scannerNormalizedNewX) + "px";
+    }
+
+    // play pause button functionality
+    let videoPlayIndicator = true;
+    playPauseButton.addEventListener("click", () => {
+      playFilledButton.classList.toggle("hidden");
+      playEmptyButton.classList.toggle("hidden");
+      pauseEmptyButton.classList.toggle("hidden");
+      if (videoPlayIndicator) {
+        // play video
+        inputVideo.play();
+        videoPlayIndicator = false;
+      } else {
+        inputVideo.pause();
+        videoPlayIndicator = true;
+      }
+    });
+
+    inputVideo.addEventListener("timeupdate", setScanner);
+
+    // get bounding x values from scanner box
+    const widthEndOffset = 12;
+    const scannerWidth = videoScanner.offsetWidth - widthEndOffset;
 
     // Make the DIV element draggable:
     dragElement(trimStartDrag);
